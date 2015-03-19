81e1fa4dff3148c052dc38d3468d4e97a35083cf
Brad Greenlee <brad@etsy.com>
2015-03-19 15:13:57 -0700

    Parse JSON into a Weather struct

--- WeatherBar/WeatherAPI.swift
+++ WeatherBar/WeatherAPI.swift
@@ -8,17 +8,47 @@
 
 import Foundation
 
+struct Weather: Printable {
+    var city: String
+    var currentTemp: Float
+    var conditions: String
+
+    var description: String {
+        return "\(city): \(currentTemp)F and \(conditions)"
+    }
+}
+
 class WeatherAPI {
     let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?units=imperial&q="
-    
+
     func fetchWeather(query: String) {
         let session = NSURLSession.sharedSession()
         let escapedQuery = query.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
         let url = NSURL(string: BASE_URL + escapedQuery!)
         let task = session.dataTaskWithURL(url!) { data, response, error in
-            let dataString = NSString(data: data, encoding: NSUTF8StringEncoding) as String
-            NSLog(dataString)
+            let weather = self.weatherFromJSONData(data)
+            NSLog("\(weather)")
         }
         task.resume()
+    }
+
+    func weatherFromJSONData(data: NSData) -> Weather? {
+        var err: NSError?
+        typealias JSONDict = [String:AnyObject]
+
+        if let json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.allZeros, error: &err) as? JSONDict {
+            var mainDict = json["main"] as JSONDict
+            var weatherList = json["weather"] as [JSONDict]
+            var weatherDict = weatherList[0]
+
+            var weather = Weather(
+                city: json["name"] as String,
+                currentTemp: mainDict["temp"] as Float,
+                conditions: weatherDict["main"] as String
+            )
+
+            return weather
+        }
+        return nil
     }
 }
